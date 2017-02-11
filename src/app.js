e7cef6f638b0f9d136045b132eea65d8ffc3d0b4
Rabin Gaire <rabingaire20@gmail.com>
2017-02-11 17:03:36 +0545

    app completed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCXLZ2qBFG_Egyn34za24Bbq4gcPqAP74w',
@@ -13,14 +15,45 @@ export default class App extends Component {
       storageBucket: 'auth-d6ca5.appspot.com',
       messagingSenderId: '157165463615'
     });
-  }
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  };
+
+  renderContent() {
+    switch(this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress = { () => firebase.auth().signOut() }>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner />
+    }
+  };
 
   render() {
     return (
       <View>
         <Header headerText = "Auth" />
-        <LoginForm />
+        <View style = { styles.contentStyle }>
+          { this.renderContent() }
+        </View>
       </View>
     );
   }
-}
+};
+
+const styles = {
+  contentStyle: {
+    marginTop: 50
+  }
+};
